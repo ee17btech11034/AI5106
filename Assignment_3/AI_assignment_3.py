22cee46b46c025d6decb00425d9c7edb89d126ab
ee17btech11034 <47558658+ee17btech11034@users.noreply.github.com>
2020-11-20 14:24:40 +0530

    Update AI_assignment_3.py

--- Assignment_3/AI_assignment_3.py
+++ Assignment_3/AI_assignment_3.py
@@ -1,52 +1,48 @@
 import math
 import numpy as np
-import matplotlib.pyplot as plt
+import csv
+import matplotlib.pyplot as plt 
+from sympy import *
 
-
-(x,y) = (0,0)     # this is just only for calculate the minors
 (x1,y1) = (1,2)   #point P
 (x2,y2) = (3,-4)  #point Q
 (x3,y3) = (5,-6)  #point R
 
-h = np.linspace(1, 21, 1000)
+P = np.array([x1,y1])
+Q = np.array([x2,y2])
+R = np.array([x3,y3])
 
-M = [[x**2 + y**2, x, y, 1], [x1**2 + y1**2, x1, y1, 1], [x2**2 + y2**2, x2, y2, 1], [x3**2 + y3**2, x3, y3, 1]]
+x = np.linspace(1, 21, 1000)
 
-def mnr(M,i,j):
-    return [row[:j] + row[j+1:] for row in (M[:i]+M[i+1:])]
+# making augmented matrix
+M =[[2*x1, 2*y1, -1, (np.linalg.norm(P))**2], [2*x2, 2*y2, -1, (np.linalg.norm(Q))**2], [2*x3, 2*y3, -1, (np.linalg.norm(R))**2]]
+M1 = Matrix(M)
+M_rred = M1.rref() 
 
-M11 = np.linalg.det(mnr(M,0,0))
-M12 = np.linalg.det(mnr(M,0,1))
-M13 = np.linalg.det(mnr(M,0,2))
-M14 = np.linalg.det(mnr(M,0,3))
+g = -1*M_rred[0][3] 
+f = -1*M_rred[0][7]
+k = M_rred[0][11]
 
-g = round(-M12/(2*M11),3)
-f = round(M13/(2*M11), 3)
-c = round(-M14/M11, 3)
-r = np.sqrt((-g-x1)**2 + (-f-y1)**2)
-
-k1 = -f + np.sqrt((100 - (h+g)**2)) 
-k2 = -f - np.sqrt((100 - (h+g)**2)) 
+k1 = 2 + np.sqrt((100 - (x-11)**2)) 
+k2 = 2 - np.sqrt((100 - (x-11)**2)) 
 fig, ax = plt.subplots()
 ax.set_aspect(1)
 
-print("centre of this circle is (x0,y0) = ", (-g,-f))
-print("constant c = ", c)
-print("radius of circle : ",r)
+print("Centre of Circle: ",(-g,-f))
 
 plt.annotate("P(1,2)", (x1,y1))
 plt.annotate("Q(3,-4)", (x2,y2))
 plt.annotate("R(5,-6)", (x3,y3))
-plt.annotate("C(11,2)", (-g,-f))
+plt.annotate("C(11,2)", (11,2))
 plt.plot(x1,y1,'o')
 plt.plot(x2,y2,'o')
 plt.plot(x3,y3,'o')
-plt.plot(-g,-f,'o')
-plt.plot(h,k1,'y',label='x^T x - 2 (11  2)x + 25 = 0')
-plt.plot(h,k2,'y')
+plt.plot(11,2,'o')
+plt.plot(x,k1,'y',label='x^T x - 2 (11  2)x + 25 = 0')
+plt.plot(x,k2,'y')
 
-plt.xlim(-g-r-2,-g+r+2)
-plt.ylim(-f-r-4,-f+r+4)
+plt.xlim(-1,23)
+plt.ylim(-10,15)
 plt.legend(loc='upper right')
 
 plt.show()
